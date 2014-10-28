080ee6ff1f0ccf700516d17b9e0eb8029ccf80f8
Rouston <r.zellat@gmail.com>
2014-10-28 21:45:53 +0100

    RPG project

---
+++ Personnage.h
@@ -0,0 +1,22 @@
+#ifndef DEF_PERSONNAGE
+#define DEF_PERSONNAGE
+
+#include <string>
+
+class Personnage
+{
+    public:
+        void recevoirDegats(int nbDegats);
+        void attaquer(Personnage &cible);
+        void boirePotionDeVie(int quantitePotion);
+        void changerArme(std::string nomNouvelleArme, int degatsNouvelleArme);
+        void estVivant();
+
+    private:
+        int m_vie;
+        int m_mana;
+        std::string m_nomArme;
+        int m_degatsArme;
+
+};
+#endif // DEF_PERSONNAGE
